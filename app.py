0abd9fe1da2bb414b6a02cc3e7cf1e6329c4e572
flora <flora90.1.10@gmail.com>
2021-05-19 03:24:27 +0800

    add crwal

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import urllib.request as req
+import bs4
+import json
+import collections
+from ..college_project.gossipg import crawl()
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('Jc+EHqvTL5jUcUL64J9GLzz8ttcXAjVjs4dCGKvZy8miPrfBEAPKbN6Kj2aCU9wCgS199Pyq7jNO8cvxF0FnAPpJjwY4Yy9XkL7QW6Q0Pofylf38z8i6apcmZIiH5qfaBYSKz9uZkwN9DuTWeA2IRQdB04t89/1O/w1cDnyilFU=')
@@ -35,6 +41,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    crawl(https://www.ptt.cc/bbs/Gossiping/index.html)
     #print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
     content = "{}: {}".format(event.source.user_id, event.message.text)
     line_bot_api.reply_message(
